8e584eab866648fe612eb0a56e48c46b8c99f787
sstonn <p_toan@gonosen.asia>
2024-01-13 16:55:49 +0700

    [LR] [Add learning curve to linear regression script]

--- helper_functions.py
+++ helper_functions.py
@@ -1,41 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def linear_regression(X, w, b):
-    return np.dot(X, w.T) + b
-
-
-def residual(X, w, b, y):
-    return linear_regression(X, w, b) - y
-
-
-def cost_function(X, w, b, y):
-    m = X.shape[0]
-    return np.sum(residual(X, w, b, y) ** 2) / (2 * m)
-
-
-def compute_gradient(X, w, b, y):
-    m, n = X.shape
-    dj_dw = np.zeros((n,))
-    dj_db = 0
-    dj_dw += np.sum(residual(X, w, b, y) * X.T, axis=1)
-    dj_db += np.sum(residual(X, w, b, y))
-    dj_dw /= m
-    dj_db /= m
-    return dj_dw, dj_db
-
-
-def gradient_descent(X, w, b, y, number_of_iterations, alpha):
-    temp_w = w.copy()
-    temp_b = b
-    for _ in range(number_of_iterations):
-        dj_dw, dj_db = compute_gradient(X, temp_w, temp_b, y)
-        temp_w -= dj_dw * alpha
-        temp_b -= dj_db * alpha
-        # print(cost_function(X, temp_w, temp_b, y))
-
-    return temp_w, temp_b
-
+dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0';
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0')
 
 def plot_learning_curve(cost_history, learning_rate):
     plt.plot(cost_history)
